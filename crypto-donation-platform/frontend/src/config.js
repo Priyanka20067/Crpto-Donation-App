1b1f76cdd9a7f5ef990184a4722fa3734a73adb9
Priyanka@20067 <priyankam18042006@gmail.com>
2025-08-08 22:24:53 +0530

    address change

--- crypto-donation-platform/frontend/src/config.js
+++ crypto-donation-platform/frontend/src/config.js
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0xYourNewContractAddress";
+export const CONTRACT_ADDRESS = "0xBf8167c7D6569a1C60EfAF91328D1d83B0a137aB";
 export const CONTRACT_ABI = [
   "function withdrawETH() public",
   "function withdrawToken(address) public",
@@ -16,3 +16,85 @@ export const SUPPORTED_TOKENS = {
     decimals: 6
   }
 };
+[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "donor",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "DonationReceived",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdrawal",
+		"type": "event"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
